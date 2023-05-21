b7b2b703088f22a38308d1914e7f03c986697786
Andrei <andrei18makrushin@gmail.com>
2023-05-21 23:21:32 +0500

    adattive code

--- src/stores/users.js
+++ src/stores/users.js
@@ -4,23 +4,14 @@ import data from '../data/data.json'
 
 export const useUsersStore = defineStore('users', () => {
   const userAll = ref(data)
+
   const country = ref('')
   const points = ref('')
 
-  watchEffect((onInvalidate) => {
-    if (!country.value && !points.value) {
-      userAll.value = data
-      return
-    }
-    const filteredUsers = data.filter(
-      (user) =>
-        (!country.value || user.country.includes(country.value)) &&
-        (!points.value || user.point.includes(points.value))
+  watchEffect(() => {
+    userAll.value = data.filter(
+      (user) => user.country.includes(country.value) && user.point.includes(points.value)
     )
-    userAll.value = filteredUsers
-    onInvalidate(() => {
-      userAll.value = data
-    })
   })
 
   return { userAll, country, points }
